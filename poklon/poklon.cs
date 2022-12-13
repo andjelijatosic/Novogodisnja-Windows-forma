c3ffabb523599b19810d23fbac971088b390a432
jovanajanjatovic <jovana.janjatovic.05@gmail.com>
2022-12-13 15:59:43 +0100

    Add files via upload
    konstruktor i private metode

---
+++ poklon/poklon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace WindowsFormsApp1
+{
+    internal class poklon
+    {
+        private Point centar;
+        private int a;
+        private Color boja_omotaca;
+        private Color boja_trake;
+
+        public poklon(Point centar, int a, Color boja_omotaca, Color boja_trake)
+        {
+            this.centar = centar;
+            this.a = a;
+            this.boja_omotaca = boja_omotaca;
+            this.boja_omotaca = boja_trake;
+        }
+
+
+
+    }
+}
