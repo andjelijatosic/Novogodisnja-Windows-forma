b3521893dc9b01091b735f1cfe242fa9d4de8841
jovanajanjatovic <jovana.janjatovic.05@gmail.com>
2022-12-14 14:31:28 +0100

    Add files via upload
    dodata medota crtaj

--- poklon/poklon.cs
+++ poklon/poklon.cs
@@ -20,7 +20,23 @@ namespace WindowsFormsApp1
             this.boja_omotaca = boja_omotaca;
             this.boja_omotaca = boja_trake;
         }
+        public poklon()
+        {
+            centar  new Point(0,0)
+        }
 
+        public void Crtaj(Graphics g)
+        {
+            Pen olovka = new Pen(Color.Black);
+
+            Point t1 = new Point(centar.X - 3 * a / 2, centar.Y + 2 * a);
+            Point t2 = new Point(centar.X + 3 * a / 2, centar.Y + 2 * a);
+            Point t3 = new Point(centar.X - 3 * a / 2, centar.Y); 
+            Point t4 = new Point(centar.X + 3 * a / 2, centar.Y);
+
+            Point[] niz1 = { t1, t2, t3, t4 };
+            g.FillPollygon(olovka, niz1);
+        }
 
 
     }
