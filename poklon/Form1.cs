e7d864ab26c21bd4bf015e3d6269039f6bb2ac40
jovanajanjatovic <jovana.janjatovic.05@gmail.com>
2022-12-26 15:08:16 +0100

    Add files via upload
    pokloni padaju

--- poklon/Form1.cs
+++ poklon/Form1.cs
@@ -17,32 +17,54 @@ namespace poklon
         {
             InitializeComponent();
         }
-        poklon p;
         Color boja1;
         Color boja2;
         Random r = new Random();
         Point polozaj;
-        int a = 50;
+        int a = 30;
+        int n = 4;
+        poklon[] Pniz = new poklon[4];
         private void Form1_Load(object sender, EventArgs e)
         {
+            boja1 = Color.FromArgb(r.Next(256), r.Next(0), r.Next(0));
+            boja2 = Color.FromArgb(r.Next(0), r.Next(240), r.Next(0));
             timer1.Enabled = true;
             timer1.Interval = 500;
-            
+            for (int br = 0; br < n; br++)
+            {
+                polozaj = new Point( r.Next(a, ClientRectangle.Width - a), 0);
+                poklon p = new poklon(polozaj, a, boja1, boja2);
+                Pniz[br] = p;
+
+            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            boja1 = Color.FromArgb(r.Next(256), r.Next(0), r.Next(0));
-            boja2 = Color.FromArgb(r.Next(0), r.Next(240), r.Next(0));
             Graphics g = CreateGraphics();
-            polozaj = new Point(r.Next(a, ClientRectangle.Width - a), r.Next(a, ClientRectangle.Height - a));
-            p = new poklon(polozaj, a , boja1, boja2);
-            p.Crtaj(g);
+            for (int br = 0; br < n; br++)
+            {
+                Pniz[br].Crtaj(g);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            Refresh();
+            Graphics g = CreateGraphics();
+            for (int br = 0; br < n; br++)
+            {
+                poklon p = new poklon();
+                p = Pniz[br];
+                Pniz[br].smanjiY(50);
+                Pniz[br].Crtaj(g);
+                Refresh();
+                if (p.getPolozaj().Y > ClientRectangle.Height)
+                {
+                    Pniz[br].smanjiY(-ClientRectangle.Height);
+                   
+                }
+            }
+
         }
     }
 }
