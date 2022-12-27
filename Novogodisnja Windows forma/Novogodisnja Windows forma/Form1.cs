791ebe60473a541c6fb32bc2585ff452c8485f10
jovanajanjatovic <jovana.janjatovic.05@gmail.com>
2022-12-27 16:00:16 +0100

    Update Form1.cs
    fgdsfgs

--- Novogodisnja Windows forma/Novogodisnja Windows forma/Form1.cs
+++ Novogodisnja Windows forma/Novogodisnja Windows forma/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,9 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Novogodisnja_Windows_forma;
-
-namespace Novogodisnja_Windows_forma
+using bezi_od_poklona;
+namespace bezi_od_poklona
 {
     public partial class Form1 : Form
     {
@@ -25,25 +24,31 @@ namespace Novogodisnja_Windows_forma
         int n = 4;
         int x;
         poklon[] Pniz = new poklon[4];
+        bool goleft;
+        bool goright;
 
         patuljak patuljak = new patuljak();
-        jelka jelkaa = new jelka(70, 200, 90);
-        korpa korp = new korpa(20, 0, 0);
+        jelka jelkaa = new jelka(70, 50, 90);
         private void Form1_Load(object sender, EventArgs e)
         {
             boja1 = Color.FromArgb(r.Next(256), r.Next(0), r.Next(0));
             boja2 = Color.FromArgb(r.Next(0), r.Next(240), r.Next(0));
             timer1.Enabled = true;
             timer1.Interval = 500;
+0;
+            timer2.Enabled = true;
+            timer2.Interval = 500;
             for (int br = 0; br < n; br++)
             {
                 polozaj = new Point(r.Next(a, ClientRectangle.Width - a), 0);
                 poklon p = new poklon(polozaj, a, boja1, boja2);
                 Pniz[br] = p;
             }
-            patuljak.Setsve(50, 100, new Point(ClientRectangle.Width-100, ClientRectangle.Height-100));
-            jelkaa = new jelka(70, ClientRectangle.Height - 30, 70);
-            korp = new korpa(x, ClientRectangle.Height - 10, 30);
+            Graphics g = CreateGraphics();
+            jelkaa = new jelka(70, ClientRectangle.Height - 30, 170);
+            jelkaa.crtaj(g);
+            patuljak.Setsve(50, 100, new Point(ClientRectangle.Width - 100, ClientRectangle.Height - 100));
+
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -60,6 +65,10 @@ namespace Novogodisnja_Windows_forma
                 {
                     Pniz[br].smanjiY(-ClientRectangle.Height);
                 }
+                if (p.getPolozaj().X == ipsilon && p.getPolozaj().Y == ClientRectangle.Width - 100)
+                {
+                    //fgndfgsnhdg
+                }
             }
         }
 
@@ -72,17 +81,51 @@ namespace Novogodisnja_Windows_forma
             }
             patuljak.nacrtaj(g);
             jelkaa.crtaj(g);
-            korp.crtaj(g);
+
+
         }
 
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            x = e.X;
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+            
         }
 
-        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
-            x = e.X;
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+            
+        }
+        int ipsilon = 200;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+          
+            if (goleft == true)
+            {
+                ipsilon -= 40;
+            }
+            else if (goright == true)
+            {
+                ipsilon += 40;
+            }
+            patuljak.Setsve(50, 100, new Point(ipsilon, ClientRectangle.Height - 100)) ;
+            Graphics g = CreateGraphics();
+            patuljak.nacrtaj(g);
+
         }
     }
 }
